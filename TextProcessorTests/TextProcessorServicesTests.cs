bd8afad57cabd48626f30f650ede8c80a84765a3
Callum Bedford <callum.a.r.bedford@gmail.com>
2025-10-08 16:21:34 +0200

    Added a test class to check that file handling works as expected

---
+++ TextProcessorTests/TextProcessorServicesTests.cs
@@ -0,0 +1,82 @@
+﻿using TextProcessor.Services;
+
+namespace TextProcessorTesting
+{
+	[TestClass]
+	public class TextProcessorServicesTests
+	{
+		private string _tempFilePath = string.Empty;
+
+		[TestInitialize]
+		public void Setup()
+		{
+			_tempFilePath = Path.GetTempFileName();
+		}
+
+		[TestCleanup]
+		public void Cleanup()
+		{
+			if (File.Exists(_tempFilePath))
+			{
+				File.Delete(_tempFilePath);
+			}
+		}
+
+		[TestMethod]
+		public void Processor_ShouldReturnString()
+		{
+			// Arrange
+			var service = new TextProcessingService();
+			string input = "hello";
+
+			// Act
+			string result = service.ProcessText(input);
+
+			// Assert
+			Assert.AreEqual("hello", result);
+		}
+	}
+
+	/// <summary>
+	/// Confirm that usage of files behaves as expected.
+	/// </summary>
+	[TestClass]
+	public class FileUtilityTests
+	{
+		private string _tempFilePath = string.Empty;
+
+		[TestInitialize]
+		public void Setup()
+		{
+			_tempFilePath = Path.GetTempFileName();
+		}
+
+		[TestCleanup]
+		public void Cleanup()
+		{
+			if (File.Exists(_tempFilePath))
+			{
+				File.Delete(_tempFilePath);
+			}
+		}
+
+		[TestMethod]
+		public async Task TempFileReadsAndWrites()
+		{
+			// Arrange
+			var content = "lorem ipsum";
+			await File.WriteAllTextAsync(_tempFilePath, content);
+
+			// Act
+			string? lines;
+			using (var reader = new StreamReader(_tempFilePath))
+			{
+				lines = await reader.ReadLineAsync();
+			}
+
+			// Assert
+			Assert.IsTrue(File.Exists(_tempFilePath));
+			Assert.AreEqual(lines, content);
+		}
+	}
+}
