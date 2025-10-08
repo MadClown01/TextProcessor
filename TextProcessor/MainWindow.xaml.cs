632a41b0b01d591e02f9cd490bb26ad88d057157
Callum Bedford <callum.a.r.bedford@gmail.com>
2025-10-08 16:41:28 +0200

    MainWindowUI creates a dialogue that gets a text file path

--- TextProcessor/MainWindow.xaml.cs
+++ TextProcessor/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using Microsoft.Win32;
+using System.Windows;
 
 namespace TextProcessorViews
 {
@@ -12,12 +13,25 @@ namespace TextProcessorViews
 		private void Process_Click(object sender, RoutedEventArgs e)
 		{
 			// Temporary stub logic
-			OutputTextBox.Text = new string(InputTextBox.Text);
+			//OutputTextBox.Text = new string(InputTextBox.Text);
+
+			var dialog = new OpenFileDialog
+			{
+				Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*",
+				Title = "Select a text file to process"
+			};
+
+			bool? result = dialog.ShowDialog();
+
+			if (result == true) // user clicked OK
+			{
+				string selectedFilePath = dialog.FileName;
+				// pass this path to your TextProcessingService
+			}
 		}
 
 		private void Clear_Click(object sender, RoutedEventArgs e)
 		{
-			InputTextBox.Clear();
 			OutputTextBox.Clear();
 		}
 	}
