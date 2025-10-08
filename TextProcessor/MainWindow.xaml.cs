2fa988a60c9072c4fc6ff1305e0a9d11b440eb50
Callum Bedford <callum.a.r.bedford@gmail.com>
2025-10-08 13:30:25 +0200

    App runs and takes text input

--- TextProcessor/MainWindow.xaml.cs
+++ TextProcessor/MainWindow.xaml.cs
@@ -1,23 +1,24 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace TextProcessor;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace TextProcessor
 {
-    public MainWindow()
-    {
-        InitializeComponent();
-    }
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Process_Click(object sender, RoutedEventArgs e)
+		{
+			// Temporary stub logic
+			OutputTextBox.Text = new string(InputTextBox.Text);
+		}
+
+		private void Clear_Click(object sender, RoutedEventArgs e)
+		{
+			InputTextBox.Clear();
+			OutputTextBox.Clear();
+		}
+	}
 }
