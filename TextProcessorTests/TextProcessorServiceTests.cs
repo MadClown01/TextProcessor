2bc30e5b4b8bda974bae706f083a8cd8ce0f89e6
Callum Bedford <callum.a.r.bedford@gmail.com>
2025-10-09 22:21:33 +0200

    Add a word counter unit test based on the task example

--- TextProcessorTests/TextProcessorServiceTests.cs
+++ TextProcessorTests/TextProcessorServiceTests.cs
@@ -118,6 +118,25 @@ namespace TextProcessor.Testing
 			Assert.AreEqual(counts["b"], 1);
 			Assert.AreEqual(counts["c"], 2);
 		}
+		public void WordCounter_HandlesTaskExample()
+		{
+			// Arrange
+			string[] tokens = new string[] { "1:1", "Adam", "Seth", "Enos", "1:2", "Cainan", "Adam", "Seth", "Iared" };
+			var counter = new WordCounter();
+
+			// Act
+			counter.CountWords(tokens);
+			IReadOnlyDictionary<string, int> counts = counter.GetCounts();
+
+			// Assert
+			Assert.AreEqual(counts["1:1"], 1);
+			Assert.AreEqual(counts["Adam"], 2);
+			Assert.AreEqual(counts["Seth"], 2);
+			Assert.AreEqual(counts["Enos"], 1);
+			Assert.AreEqual(counts["1:2"], 1);
+			Assert.AreEqual(counts["Cainan"], 1);
+			Assert.AreEqual(counts["Iared"], 1);
+		}
 	}
 
 	/// <summary>
