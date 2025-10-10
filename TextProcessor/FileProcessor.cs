f067a03a3e0ab77fd5c828613913f9d15eaf9845
Callum Bedford <callum.a.r.bedford@gmail.com>
2025-10-10 11:35:25 +0200

    Added File Processor class to wrap other services

---
+++ TextProcessor/FileProcessor.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using TextProcessor.Interfaces;
+
+namespace TextProcessor.Services
+{
+	/// <summary>
+	/// Responsible for reading, tokenising, counting.
+	/// Accepts a file path, tokeniser, word counter.
+	/// Reports progress via IProgress<double>.
+	/// Supports cancellation via CancellationToken.
+	/// </summary>
+	public class FileProcessor : IFileProcessor
+	{
+		private readonly IFileReader _fileReader;
+		private readonly ITokeniser _tokeniser;
+
+		public FileProcessor(IFileReader fileReader, ITokeniser tokeniser)
+		{
+			_fileReader = fileReader;
+			_tokeniser = tokeniser;
+		}
+
+		public async Task<IReadOnlyDictionary<string, int>> ProcessFileAsync(
+			string filePath,
+			IProgress<double> progress,
+			CancellationToken token)
+		{
+			var wordCounter = new WordCounter();
+			var totalBytes = new FileInfo(filePath).Length; // Does this need to be async?
+
+			//var bytesRead = reader.BaseStream.Position;
+			//progress = (double)bytesRead / totalBytes;
+
+			await foreach (var line in _fileReader.ReadLinesAsync(filePath).WithCancellation(token))
+			{
+				var words = _tokeniser.TokeniseLine(line);
+				wordCounter.CountWords(words);
+
+				var bytesRead = 1;//reader.BaseStream.Position;
+				progress?.Report((double)bytesRead / totalBytes * 100);
+			}
+
+			return wordCounter.GetCounts();
+		}
+	}
+}
---
+++ TextProcessor/IFileProcessor.cs
@@ -0,0 +1,7 @@
+﻿namespace TextProcessor.Interfaces
+{
+	public interface IFileProcessor
+	{
+		public Task<IReadOnlyDictionary<string, int>> ProcessFileAsync(string filePath, IProgress<double> progress, CancellationToken token);
+	}
+}
