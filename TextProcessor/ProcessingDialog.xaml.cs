3a13e6be2b3fd7ff34fa45b7c42e8f4015757a59
Callum Bedford <callum.a.r.bedford@gmail.com>
2025-10-11 13:27:23 +0200

    Removed unused code from ProcessingDialog

--- TextProcessor/ProcessingDialog.xaml.cs
+++ TextProcessor/ProcessingDialog.xaml.cs
@@ -7,17 +7,19 @@ namespace TextProcessor.Views
 	{
 		private readonly CancellationTokenSource _cts = new();
 		public CancellationToken CancellationToken => _cts.Token;
-		public IProgress<double> Progress { get; }
 
 		public ProcessingDialog()
 		{
 			InitializeComponent();
-			Progress = new Progress<double>(value => ProgressBar.Value = value);
 		}
 
 		public void Report(long bytesRead, long totalBytes)
 		{
-			double percent = totalBytes == 0 ? 0 : (bytesRead / (double)totalBytes) * 100;
+			double percent = 0;
+			if (totalBytes > 0)
+			{
+				percent = (bytesRead / (double)totalBytes) * 100;
+			}
 
 			ProgressBar.Value = percent;
 			StatusText.Text = $"{bytesRead:N0} / {totalBytes:N0} bytes processed ({percent:F1}%)";
